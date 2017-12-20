7591bef448df4cd1ea921a769df459f8701cff4d
Adnan Mackic <amackic@gmail.com>
2017-12-19 19:29:43 -0500

    added search page and validation

--- main.py
+++ main.py
@@ -13,15 +13,22 @@ def about():
 
 @app.route("/search", methods=['POST', 'GET'])
 def search():
-    searchParam = "nothing"
+
+    makes=[
+        'Any Make', 'Alfa Romeo', 'AMC', 'Aston Martin', 'Audi', 'Bentley', 'BMW', 'Bugatti', 'Buick', 'Cadillac', 'Chevrolet', 'Chrysler', 'Daewoo',
+        'Datsun', 'DeLorean', 'Dodge', 'Eagle', 'Ferrari', 'FIAT', 'Fisker', 'Ford', 'Freightliner', 'Genesis', 'Geo', 'GMC', 'Honda', 'HUMMER',
+        'Hyundai', 'INFINITI', 'Isuzu', 'Jaguar', 'Jeep', 'Kia', 'Lamborghini', 'Land Rover', 'Lexus', 'Lincoln', 'Lotus', 'Maserati', 'Maybach', 'Mazda', 'McLaren',
+        'Mercedes-Benz', 'Mercury', 'MINI', 'Mitsubishi', 'Nissan', 'Oldsmobile', 'Plymouth', 'Pontiac', 'Porsche', 'RAM', 'Rolls-Royce', 'Saab', 'Saturn', 'Scion',
+        'smart', 'SRT', 'Subaru', 'Suzuki', 'Tesla', 'Toyota', 'Volkswagen', 'Volvo', 'Yugo']
+
     if request.method == 'POST':
-        searchParam = request.form["carType"]
+        searchParam = request.form["price"]
         print(searchParam)
 
     if request.method == 'GET':
         searchParam = request.args.get("q")
         print(searchParam);
 
-    return render_template("/search.html")
+    return render_template("/search.html", makes=makes)
 
 app.run()
--- static/search.js
+++ static/search.js
@@ -1,3 +1,11 @@
-function evalInput(){
-    alert("hi");
+function isNumeric(valueElement, messageElement){
+    debugger;
+    var value = document.getElementById(valueElement).value;
+    if (!(!isNaN(parseFloat(value)) && isFinite(value))){
+        document.getElementById('searchButton').disabled = true;
+        document.getElementById(messageElement).innerHTML = "Please enter a valid number";
+    }else{
+        document.getElementById('searchButton').disabled = false;
+        document.getElementById(messageElement).innerHTML = "";
+    }
 }
